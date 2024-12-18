143a62125f599e36571ca671df7a2b64be364de1
Marouane Boufarouj <104838272+grainme@users.noreply.github.com>
2024-12-18 15:30:43 +0100

    Delete rusty_grep/target directory

--- rusty_grep/target/debug/deps/rusty_grep-087db65f990f9eca.d
+++
@@ -1,5 +0,0 @@
-/home/grainme/codespace/Rust/rusty_grep/rusty_grep/target/debug/deps/librusty_grep-087db65f990f9eca.rmeta: src/main.rs
-
-/home/grainme/codespace/Rust/rusty_grep/rusty_grep/target/debug/deps/rusty_grep-087db65f990f9eca.d: src/main.rs
-
-src/main.rs:
