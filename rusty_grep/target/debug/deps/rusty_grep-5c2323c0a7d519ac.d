d48043a22c13f72d2550edc63f39cc71be4e3d7d
Marouane Boufarouj <104838272+grainme@users.noreply.github.com>
2024-12-18 15:31:30 +0100

    Delete rusty_grep/target directory

--- rusty_grep/target/debug/deps/rusty_grep-5c2323c0a7d519ac.d
+++
@@ -1,5 +0,0 @@
-/home/grainme/codespace/Rust/rusty_grep/rusty_grep/target/debug/deps/librusty_grep-5c2323c0a7d519ac.rmeta: src/main.rs
-
-/home/grainme/codespace/Rust/rusty_grep/rusty_grep/target/debug/deps/rusty_grep-5c2323c0a7d519ac.d: src/main.rs
-
-src/main.rs:
