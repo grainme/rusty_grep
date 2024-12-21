58b4c34377bb7fb95bb2b67e29ddf9b3a3a5eb24
Marouane Boufarouj <104838272+grainme@users.noreply.github.com>
2024-12-21 14:39:42 +0100

    Delete rusty_grep/target directory

--- rusty_grep/target/debug/deps/rusty_grep-890d5b3e20008cda.d
+++
@@ -1,9 +0,0 @@
-/home/grainme/codespace/rusty_grep/rusty_grep/target/debug/deps/librusty_grep-890d5b3e20008cda.rmeta: src/main.rs Cargo.toml
-
-/home/grainme/codespace/rusty_grep/rusty_grep/target/debug/deps/rusty_grep-890d5b3e20008cda.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
