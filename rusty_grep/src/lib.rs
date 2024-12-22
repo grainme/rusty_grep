749cd1270bf64f634c99d504f082b77f68c00443
grainme <marouanebouf3@gmail.com>
2024-12-22 18:35:04 +0100

    Rusty Grep finished

--- rusty_grep/src/lib.rs
+++ rusty_grep/src/lib.rs
@@ -1,13 +1,23 @@
-use std::{error::Error, fs, result};
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(&config.file_path)?;
-    Ok(())
-}
+use std::{env, error::Error, fs};
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(&config.file_path)?;
+
+    let search_result = match config.ignore_case {
+        true => search_case_insensitive(&config.query, &content),
+        false => search(&config.query, &content),
+    };
+
+    for line in search_result {
+        println!("{line}");
+    }
+    Ok(())
 }
 
 impl Config {
@@ -18,30 +28,60 @@ impl Config {
         Ok(Config {
             query: args[1].clone(),
             file_path: args[2].clone(),
+            ignore_case: env::var("IGNORE_CASE").is_ok(),
         })
     }
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result: Vec<&str> = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line.trim());
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut res: Vec<&str> = Vec::new();
+    for line in content.lines() {
+        if line.contains(&query) {
+            res.push(line);
         }
     }
-    result
+    res
+}
+
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut res: Vec<&str> = Vec::new();
+    let query = query.to_lowercase();
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            res.push(line);
+        }
+    }
+    res
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
-            Rust:
-            safe, fast, productive.
-            Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- rusty_grep/src/main.rs
+++ rusty_grep/src/main.rs
@@ -17,11 +17,13 @@ fn main() {
 
     // unwrap_or_else calls the code in closur (TBS chp 13)
     let config: Config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     // if let instead of unwrap_or_else because run doesn't return an OK :)
     if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
