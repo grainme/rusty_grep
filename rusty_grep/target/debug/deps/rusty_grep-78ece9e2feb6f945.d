04f0be4f183d621ae2c7328b1211f0a8e2c05bfd
grainme <marouanebouf3@gmail.com>
2025-01-11 22:03:24 +0100

    working with iterators, fix clone

--- rusty_grep/target/debug/deps/rusty_grep-78ece9e2feb6f945.d
+++
@@ -1,5 +0,0 @@
-/home/grainme/codespace/rusty_grep/rusty_grep/target/debug/deps/librusty_grep-78ece9e2feb6f945.rmeta: src/main.rs
-
-/home/grainme/codespace/rusty_grep/rusty_grep/target/debug/deps/rusty_grep-78ece9e2feb6f945.d: src/main.rs
-
-src/main.rs:
--- rusty_grep/target/debug/deps/rusty_grep-ebd9573949913c6a.d
+++
@@ -1,5 +0,0 @@
-/home/grainme/codespace/rusty_grep/rusty_grep/target/debug/deps/librusty_grep-ebd9573949913c6a.rmeta: src/lib.rs
-
-/home/grainme/codespace/rusty_grep/rusty_grep/target/debug/deps/rusty_grep-ebd9573949913c6a.d: src/lib.rs
-
-src/lib.rs:
